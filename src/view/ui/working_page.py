bb1d289e219f550f085c2be021811c23612354e8
cooky922 <desmondbongcawel922@gmail.com>
2026-02-21 21:01:16 +0800

    move `RowHoverDelegate` and `TableHeader` to the `components`

--- src/view/ui/working_page.py
+++ src/view/ui/working_page.py
@@ -26,7 +26,14 @@ from src.utils.constants import Constants
 from src.utils.styles import Styles
 from src.utils.icon_loader import IconLoader
 
-from src.view.components import TitleLabel, InfoLabel, ToggleBox, Card
+from src.view.components import (
+    TitleLabel, 
+    InfoLabel, 
+    ToggleBox, 
+    Card,
+    RowHoverDelegate,
+    TableHeader
+)
 from src.view.ui.login_page import UserRole
 
 class DirectoryToggleBox(ToggleBox):
@@ -110,95 +117,7 @@ class TableModel(QAbstractTableModel):
     def headerData(self, section, orientation, role = Qt.ItemDataRole.DisplayRole):
         if role == Qt.ItemDataRole.DisplayRole and orientation == Qt.Orientation.Horizontal:
             return self._db.get_column_display_name(self._headers[section]).upper()
-        return None
-    
-class RowHoverDelegate(QStyledItemDelegate):
-    def __init__(self, table):
-        super().__init__(table)
-        self.table = table
-        self.hovered_row = -1
-
-        self.table.setMouseTracking(True)
-        self.table.entered.connect(self.on_entered)
-        self.table.viewport().installEventFilter(self)
-
-    def on_entered(self, index):
-        self.hovered_row = index.row()
-        self.table.viewport().update()
-
-    def eventFilter(self, obj, event):
-        if obj == self.table.viewport() and event.type() == QEvent.Type.Leave:
-            self.hovered_row = -1
-            self.table.viewport().update()
-        return super().eventFilter(obj, event)
-
-    def paint(self, painter, option, index):
-        if index.row() == self.hovered_row:
-            painter.fillRect(option.rect, QColor(0, 0, 0, 26))
-        super().paint(painter, option, index)
-
-class TableHeader(QHeaderView):
-    def __init__(self, orientation, parent=None):
-        super().__init__(orientation, parent)
-        self.setMouseTracking(True)
-
-    def mouseMoveEvent(self, event):
-        super().mouseMoveEvent(event)
-        logical_index = self.logicalIndexAt(event.pos())
-        if logical_index == -1:
-            self.viewport().setCursor(Qt.CursorShape.ArrowCursor)
-            return
-        x = event.pos().x()
-        left_edge = self.sectionViewportPosition(logical_index)
-        right_edge = left_edge + self.sectionSize(logical_index)
-        
-        on_left_divider = (x - left_edge) <= 5 and logical_index > 0
-        on_right_divider = (right_edge - x) <= 5 and logical_index < self.count() - 1
-        
-        if on_left_divider or on_right_divider:
-            self.viewport().setCursor(Qt.CursorShape.SplitHCursor)
-        else:
-            self.viewport().setCursor(Qt.CursorShape.PointingHandCursor)
-
-    def leaveEvent(self, event):
-        super().leaveEvent(event)
-        self.viewport().setCursor(Qt.CursorShape.ArrowCursor)
-
-    def paintSection(self, painter, rect, logicalIndex):
-        super().paintSection(painter, rect, logicalIndex)
-        painter.save()
-
-        if logicalIndex < self.count() - 1:
-            pen = painter.pen()
-            pen.setColor(QColor('#dddddd')) 
-            pen.setWidth(2)
-            painter.setPen(pen)
-
-            x = rect.right()
-            y_top = rect.top() + 15
-            y_bottom = rect.bottom() - 15
-            
-            painter.drawLine(x, y_top, x, y_bottom)
-        
-        if self.isSortIndicatorShown() and self.sortIndicatorSection() == logicalIndex:
-            arrow_pen = QPen(QColor('#888888'), 1.5) 
-            arrow_pen.setCapStyle(Qt.PenCapStyle.RoundCap)
-            arrow_pen.setJoinStyle(Qt.PenJoinStyle.RoundJoin)
-            painter.setPen(arrow_pen)
-
-            center_y = rect.top() + (rect.height() // 2)
-            arrow_x = rect.right() - 18 
-
-            # Smaller chevron math: Width = 8px (x to x+4 to x+8), Height = 4px
-            if self.sortIndicatorOrder() == Qt.SortOrder.AscendingOrder:
-                painter.drawLine(arrow_x, center_y + 2, arrow_x + 4, center_y - 2)
-                painter.drawLine(arrow_x + 4, center_y - 2, arrow_x + 8, center_y + 2)
-            else:
-                painter.drawLine(arrow_x, center_y - 2, arrow_x + 4, center_y + 2)
-                painter.drawLine(arrow_x + 4, center_y + 2, arrow_x + 8, center_y - 2)
-
-        painter.restore()
-        
+        return None        
 
 class Table(QWidget):
     def __init__(self):
@@ -501,7 +420,7 @@ class TableCard(Card):
         self.tool_bar.search_bar.blockSignals(False)
 
         # Reset logical state
-        self.search_text = ""
+        self.search_text = ''
         self.current_page = 0
         self.foot_bar.pagination.current_page = 0
 
